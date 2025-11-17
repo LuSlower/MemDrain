b4cff717215f24c793b3a9efd2856b9a301467ae
SirSlower <148411728+LuSlower@users.noreply.github.com>
2025-11-17 17:05:37 -0500

    Update MemDrain.cpp

--- MemDrain/MemDrain.cpp
+++ MemDrain/MemDrain.cpp
@@ -1,12 +1,12 @@
 #include "MemDrain.h"
 
-// Definir una estructura para asociar opciones con funciones
+
 typedef struct {
     const char* option;
     void (*action)(int argc, char* argv[]);
 } OptionAction;
 
-// Funciones para cada accion
+
 void help_action(int argc, char* argv[]) {
     printf("use: md.exe <Argument> <Parameter>\n");
     printf("\n  -ws <IM> <-r>   |   Drain WorkingSet");
@@ -20,13 +20,13 @@ void help_action(int argc, char* argv[]) {
 }
 void working_set_action(int argc, char* argv[]){
 
-    // Verificar parametros adicionales
+    
     if (argc >= 3){
-        //obtener PID
+        
         DWORD PID = GetPID(argv[2]);
         HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_SET_QUOTA, FALSE, PID);
         EmptyWorkingSet(hProcess);
-        // Establecerlo en todos los procesos hijos
+        
         if (argc == 4 && strcmp(argv[3], "-r") == 0){
             DWORD ChildPIDs[64] = {0};
             DWORD NumProcesses = GetChildProcesses(PID, ChildPIDs);
@@ -42,10 +42,10 @@ void working_set_action(int argc, char* argv[]){
         return;
     }
 
-    // Habilitar privilegio
+    
     EnablePrivilege(GetCurrentProcessId(), SE_PROF_SINGLE_PROCESS_NAME);
 
-    // Liberar todo el conjunto de trabajo de la memoria
+    
     SYSTEM_MEMORY_LIST_COMMAND ws;
     ws = MemoryEmptyWorkingSets;
     NtSetSystemInformation(SystemMemoryListInformation, &ws, sizeof(ws));
@@ -55,10 +55,10 @@ void working_set_action(int argc, char* argv[]){
 
 void system_working_set_action(int argc, char* argv[]){
 
-    // Habilitar privilegio
+    
     EnablePrivilege(GetCurrentProcessId(), SE_INCREASE_QUOTA_NAME);
 
-    // Liberar System WS
+    
     SYSTEM_FILECACHE_INFORMATION sfi;
     SecureZeroMemory(&sfi, sizeof(sfi));
     sfi.Flags = QUOTA_LIMITS_HARDWS_MIN_DISABLE;
@@ -71,10 +71,10 @@ void system_working_set_action(int argc, char* argv[]){
 
 void modified_page_list_action(int argc, char* argv[]){
 
-    //Habilitar privilegio
+    
     EnablePrivilege(GetCurrentProcessId(), SE_PROF_SINGLE_PROCESS_NAME);
 
-    // Liberar lista de paginas modificadas
+    
     SYSTEM_MEMORY_LIST_COMMAND mpl;
     mpl = MemoryFlushModifiedList;
     NtSetSystemInformation(SystemMemoryListInformation, &mpl, sizeof(mpl));
@@ -85,10 +85,10 @@ void modified_page_list_action(int argc, char* argv[]){
 
 void combine_memory_list_action(int argc, char* argv[]){
 
-    //Habilitar privilegio
+    
     EnablePrivilege(GetCurrentProcessId(), SE_PROF_SINGLE_PROCESS_NAME);
 
-    // Liberar lista de paginas combinadas
+    
     MEMORY_COMBINE_INFORMATION_EX mcl;
     SecureZeroMemory(&mcl, sizeof(mcl));
     mcl.Handle = NULL;
@@ -101,15 +101,15 @@ void combine_memory_list_action(int argc, char* argv[]){
 
 void stanby_list_action(int argc, char* argv[]){
 
-    //Habilitar privilegio
+    
     EnablePrivilege(GetCurrentProcessId(), SE_PROF_SINGLE_PROCESS_NAME);
 
-    // Liberar lista de espera
+    
     SYSTEM_MEMORY_LIST_COMMAND sl;
     sl = MemoryPurgeStandbyList;
     NtSetSystemInformation(SystemMemoryListInformation, &sl, sizeof(sl));
     if (argc > 2){
-        // Liberar lista de espera de baja prioridad
+        
         SYSTEM_MEMORY_LIST_COMMAND sl0;
         sl0 = MemoryPurgeLowPriorityStandbyList;
         NtSetSystemInformation(SystemMemoryListInformation, &sl0, sizeof(sl0));
@@ -121,7 +121,7 @@ void stanby_list_action(int argc, char* argv[]){
 }
 
 void registry_hives_action(int argc, char* argv[]){
-    // Vaciar colmenas (hives) de registro
+    
     NtSetSystemInformation (SystemRegistryReconciliationInformation, NULL, 0);
     printf("Sucess");
     return;
@@ -129,16 +129,16 @@ void registry_hives_action(int argc, char* argv[]){
 
 void all_action(int argc, char* argv[]){
 
-    // Habilitar privilegio
+    
     EnablePrivilege(GetCurrentProcessId(), SE_PROF_SINGLE_PROCESS_NAME);
     EnablePrivilege(GetCurrentProcessId(), SE_INCREASE_QUOTA_NAME);
 
-    // Liberar todo el conjunto de trabajo de la memoria
+    
     SYSTEM_MEMORY_LIST_COMMAND ws;
     ws = MemoryEmptyWorkingSets;
     NtSetSystemInformation(SystemMemoryListInformation, &ws, sizeof(ws));
 
-    // Liberar System WS
+    
     _SYSTEM_FILECACHE_INFORMATION sfi;
     SecureZeroMemory(&sfi, sizeof(sfi));
     sfi.Flags = QUOTA_LIMITS_HARDWS_MIN_DISABLE;
@@ -146,12 +146,12 @@ void all_action(int argc, char* argv[]){
     sfi.MinimumWorkingSet = (SIZE_T) -1;
     NtSetSystemInformation (SystemFileCacheInformationEx, &sfi, sizeof (sfi));
 
-    // Liberar lista de paginas modificadas
+    
     SYSTEM_MEMORY_LIST_COMMAND mpl;
     mpl = MemoryFlushModifiedList;
     NtSetSystemInformation(SystemMemoryListInformation, &mpl, sizeof(mpl));
 
-    // Liberar lista de paginas combinadas
+    
     _MEMORY_COMBINE_INFORMATION_EX mcl;
     SecureZeroMemory(&mcl, sizeof(mcl));
     mcl.Handle = NULL;
@@ -159,17 +159,17 @@ void all_action(int argc, char* argv[]){
     mcl.PagesCombined = 0;
     NtSetSystemInformation(SystemCombinePhysicalMemoryInformation, &mcl, sizeof(mcl));
 
-    // Liberar lista de espera
+    
     SYSTEM_MEMORY_LIST_COMMAND sl;
     sl = MemoryPurgeStandbyList;
     NtSetSystemInformation(SystemMemoryListInformation, &sl, sizeof(sl));
 
-    // Liberar lista de espera de baja prioridad
+    
     SYSTEM_MEMORY_LIST_COMMAND sl0;
     sl0 = MemoryPurgeLowPriorityStandbyList;
     NtSetSystemInformation(SystemMemoryListInformation, &sl0, sizeof(sl0));
 
-    // Vaciar colmenas (hives) de registro
+    
     NtSetSystemInformation(SystemRegistryReconciliationInformation, NULL, 0);
 
     printf("Sucess");
@@ -177,7 +177,7 @@ void all_action(int argc, char* argv[]){
 
 }
 
-// Definir las opciones y las funciones asociadas
+
 OptionAction option_actions[] = {
     {"help", help_action},
     {"-ws", working_set_action},
@@ -187,10 +187,10 @@ OptionAction option_actions[] = {
     {"-sl", stanby_list_action},
     {"-rh", registry_hives_action},
     {"-all", all_action},
-    {NULL, NULL} // ultimo elemento
+    {NULL, NULL} 
 };
 
-//buscar parametro que coincida
+
 int main(int argc, char* argv[]) {
 
     if (argc < 2) {
@@ -204,8 +204,9 @@ int main(int argc, char* argv[]) {
             return 0;
         }
     }
-    // Si la opción no coincide con ninguna acción conocida, mostrar ayuda
+    
     help_action(argc, argv);
     return 0;
 }
+
 
